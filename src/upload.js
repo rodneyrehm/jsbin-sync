cb5811b8e0428bc670074ccb67e4810d055db2da
Rodney Rehm <mail+github@rodneyrehm.de>
2015-11-13 17:25:45 +0100

    fix(upload): properly map files and result states

--- src/upload.js
+++ src/upload.js
@@ -17,32 +17,28 @@ module.exports = function(files, options) {
     endpoint: options.endpoint,
   });
 
-  var pooledUpload = function(data) {
-    var results = [];
-    var returnResults = () => results;
-    var collectResult = res => results.push(res);
+  var results = {};
+  var returnResults = () => results;
 
+  var pooledUpload = function(data) {
     var generatePromiseForPool = function() {
       if (!data.length) {
         return null;
       }
 
       var entry = data.shift();
+      var collectResult = function(res) {
+        results[entry] = res;
+      };
+
       return fileToBin(entry, jsbin, options)
         .then(collectResult);
     };
 
     var pool = new PromisePool(generatePromiseForPool, options.concurrency);
     return pool.start().then(returnResults);
-  }
+  };
 
-  return pooledUpload(files.slice(0)).then(function(states) {
-    var map = {};
-    states.forEach(function(state, index) {
-      var file = files[index];
-      map[file] = state;
-    });
-
-    return map;
-  });
+  return pooledUpload(files.slice(0))
+    .then(returnResults);
 };
